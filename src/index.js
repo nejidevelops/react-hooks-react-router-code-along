c1a196c1312b6a73f9758d3b4926da3ac2283ae6
NewtonOmbese <newtonombese1@gmail.com>
2022-07-22 15:02:29 +0300

    commit tree 509eb3f59f7871c36200dc268a1cef9cf8d6c065

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function Home() {
   return (
@@ -9,8 +11,44 @@ function Home() {
   );
 }
 
-function App() {
-  return <Home />;
+function About(){
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Login(){
+  return(
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
